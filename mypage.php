18949b29f27ac00e2a258901975c2deb5fc4e57f
isshin <isshintakatsusaido@gmail.com>
2016-09-14 15:18:53 +0800

    fix css

--- mypage.php
+++ mypage.php
@@ -34,12 +34,14 @@
     $test = count($testfollow);
 ?>
 <!DOCTYPE html>
-<meta charset="UTF-8">
+
 <html>
 <head>
+    <meta charset="UTF-8">
 	<title>divinglog</title>
     <link href="assets/css/bootstrap.css" rel="stylesheet">
     <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet">
+    <link href="assets/css/custom.css" rel="stylesheet">
 </head>
 <body>
 
