d60c7835f50f09db793eb4ef1554aace029d4fda
鈴木健人 <13bn075c@rikkyo.ac.jp>
2016-09-14 15:01:53 +0800

    footer.php作成開始。

---
+++ footer.php
@@ -0,0 +1,44 @@
+<?php
+    session_start();
+    require('dbconnect.php');
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/custom.css">
+    <title></title>
+</head>
+<body>
+</body>
+<footer>
+    <div class="container text-center">
+        <hr/>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="col-md-3">
+                    <ul class="nav nav-pills nav-stacked">
+                        <li><a href="http://localhost/diving_log/home.php">トップページ</a></li>
+                    </ul>
+                </div>
+                 <div class="col-md-3">
+                    <ul class="nav nav-pills nav-stacked">
+                        <li><a href="http://localhost/diving_log/mypage.php">マイページ</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-lg-12">
+                <ul class="nav nav-pills nav-justified">
+                    <li><a href="/">© 2013 Company Name.</a></li>
+                    <li><a href="#">Terms of Service</a></li>
+                    <li><a href="#">Privacy</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+</html>
